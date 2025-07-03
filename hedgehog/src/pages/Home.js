5134d95a9aac82238dbd4722e40e42b3fa54a354
gpdbs9409 <38944609+gpdbs9409@users.noreply.github.com>
2025-07-03 13:33:59 +0900

    Delete hedgehog directory

--- hedgehog/src/pages/Home.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ExchangeDashboard_1 = require("../components/ExchangeDashboard");
-var Home = function () {
-    return <ExchangeDashboard_1.default />;
-};
-exports.default = Home;
--- hedgehog/src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-import ExchangeDashboard from '../components/ExchangeDashboard';
-
-const Home = () => {
-  return <ExchangeDashboard />;
-};
-
-export default Home; 
